a31973838cfb5b50abab298ebe606c02451f9c2b
techi <michal.vrchota@gmail.com>
2012-04-23 12:39:10 +0200

    Firebug logger

--- src/application/models/DoctrineSqlLogger.php
+++ src/application/models/DoctrineSqlLogger.php
@@ -2,8 +2,21 @@
 
 class DoctrineSqlLogger implements Doctrine\DBAL\Logging\SQLLogger
 {
+    /**
+     *
+     * @var Zend_Log
+     */
+    
+    private $logger;
+    
+    public function __construct()
+    {
+        $this->logger = new Zend_Log(new Zend_Log_Writer_Firebug());
+    }
+    
     public function startQuery($sql, array $params = null, array $types = null)
     {
+        $this->logger->log($sql, Zend_Log::INFO);
         //echo $sql."<br>";
     }
     
