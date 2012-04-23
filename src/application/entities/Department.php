5e9bea9652b8880a1c8b02dc96bce758227d2b21
techi <michal.vrchota@gmail.com>
2012-04-23 14:47:56 +0200

    Department

---
+++ src/application/entities/Department.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * @Entity
+ * @Table(name="departments")
+ */
+class Department {
+    
+    
+    /**
+     * @Id
+     * @GeneratedValue
+     * @Column(type="integer")
+     * @var integer
+     */
+    protected $id;
+    
+    /**
+     * @Column(type="string")
+     * @var string
+     */
+    protected $name;
+    
+    /**
+     * @ManyToOne(targetEntity="Employee")
+     * @JoinColumn(name="employee_id",referencedColumnName="id",nullable=false)
+     * @var Employee
+     */
+    
+    protected $boss;
+    
+    
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getBoss() {
+        return $this->boss;
+    }
+
+    public function setBoss(Employee $boss) {
+        $this->boss = $boss;
+    }
+
+            
+    /**
+     *
+     * @return integer
+     */
+
+    public function getId()
+    {
+        return $this->id;
+    }    
+}
