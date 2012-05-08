3c7eb95b1ac9295226331b4dcb215c098a7959fc
Vašek Chromický <xchrv03@vse.cz>
2012-05-08 15:47:45 +0200

    Přidělování výkonových bodů implementováno

---
+++ src/application/controllers/PerformanceCreditsAdminController.php
@@ -0,0 +1,66 @@
+<?php
+
+class PerformanceCreditsAdminController extends Controller_Default
+{
+    /** @var Service_PerformanceCreditsAdmin */
+    private $performanceCreditsAdminService;
+
+    /** @var Service_Employee */
+    private $employeeService;
+
+    public function init() {
+        parent::init();
+
+        if (!$this->loggedSuperiorEmployee) {
+            throw new Exception("Unauthorized");
+        }
+
+        $this->performanceCreditsAdminService = new Service_PerformanceCreditsAdmin($this->em);
+        $this->employeeService = new Service_Employee($this->em);
+    }
+
+    public function indexAction()
+    {
+         $employeeList = $this->performanceCreditsAdminService->getEmployeeList($this->loggedSuperiorEmployee->getId());
+         $this->view->employeeList = $employeeList;
+    }
+
+    public function addAction()
+    {
+        $request = $this->getRequest();
+        $employeeId = $request->getParam('userId');
+        $employeeInfo = $this->employeeService->getEmployeeInfo($employeeId);
+
+        $adminForm = new Form_CreditsAdmin(
+            $this->loggedSuperiorEmployee->getSuperiorBalance()
+        );
+
+        $adminForm->prepare();
+
+		$showConfirmDialog = false;
+        if ($this->_request->isPost()) {
+            if ($this->_getParam('buttoncancel')) {
+                $this->_helper->redirector->goto('index');
+            }
+            if ($adminForm->isValid($this->_request->getPost())) {
+				$confirmed = ($this->getRequest()->getPost('hiddenconfirm') == 'true');
+				if ($confirmed) {
+					$this->performanceCreditsAdminService->givePerformanceCredits(
+						$this->loggedSuperiorEmployee->getId(),
+						$employeeId,
+						$adminForm->getValue('creditsAmount'),
+						$adminForm->getValue('note')
+					);
+					$this->_helper->redirector->goto('index');
+				}
+				else {
+					$showConfirmDialog = true;
+				}
+            }
+        }
+
+		$this->view->showConfirmDialog = $showConfirmDialog;
+        $this->view->adminForm = $adminForm;
+        $this->view->employeeInfo = $employeeInfo;
+    }
+}
---
+++ src/application/models/Service/PerformanceCreditsAdmin.php
@@ -0,0 +1,53 @@
+<?php
+
+class Service_PerformanceCreditsAdmin
+{
+    /** @var Doctrine\ORM\EntityManager */
+    private $em;
+
+    public function __construct(\Doctrine\ORM\EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function getEmployeeList($superiorEmployeeUserId)
+    {
+         $query = $this->em->createQuery("
+            SELECT e
+            FROM Employee e
+            JOIN e.superiorEmployee se
+            WHERE se.id = ?1
+        ");
+
+        $query->setParameter(1, $superiorEmployeeUserId);
+
+        $employees = array();
+
+        foreach ($query->getArrayResult() as $employee) {
+            $employees[] = array(
+                'employeeName' => $employee['name'],
+                'employeeId' => $employee['id']
+            );
+        }
+
+        return $employees;
+    }
+
+    /**
+     * @todo Process $note
+     */
+    public function givePerformanceCredits($supperiorEmployeeUserId, $employeeUserId, $creditsAmount, $note)
+    {
+        $this->em->beginTransaction();
+        $employee = $this->em->find('Employee', $employeeUserId);
+        $employee->setBalance($employee->getBalance() + $creditsAmount);
+        $this->em->persist($employee);
+
+        $supperiorEmployee = $this->em->find('SuperiorEmployee', $supperiorEmployeeUserId);
+        $supperiorEmployee->setSuperiorBalance($supperiorEmployee->getSuperiorBalance() - $creditsAmount);
+        $this->em->persist($supperiorEmployee);
+
+        $this->em->commit();
+        $this->em->flush();
+    }
+}
